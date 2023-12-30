a378e97eaa1f94d8c27ca82fac82b9b89db3b90a
amiralzd <123456amir78900@gmail.com>
2023-12-30 18:50:44 +0330

    main

--- main.js
+++ main.js
@@ -11,5 +11,51 @@ $(document).ready(function () {
 
     $('.container-ToUp').click(function() {
         $('html, body').animate({ scrollTop: 0 }, 'slow');
-      });
+    });
+
+
+
 });
+
+
+// main
+
+$(document).ready(function () {
+    // $('.owl-carousel').owlCarousel({
+    //     items:1,
+    //     rtl:true,
+    //     loop:true,
+    //     margin:10,
+    //     // nav:true,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         600:{
+    //             items:3
+    //         },
+    //         1000:{
+    //             items:5
+    //         }
+    //     }
+    // })
+
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        rtl:true,
+ 
+        navigation : true, // Show next and prev buttons
+   
+        slideSpeed : 300,
+        paginationSpeed : 400,
+   
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+   
+    });
+});
+
+// main
